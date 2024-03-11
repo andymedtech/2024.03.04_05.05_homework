0f0aa4e49efa8ac043bac50056096b519509146b
Andrey Belomyttsev <andymedtech@gmail.com>
2024-03-11 16:31:43 +0300

    refactor: code assembly

--- index.js
+++ index.js
@@ -1,92 +1,70 @@
-class Plants {
-  // DataBase
-  // user = [
-  //   { inn: 1, name: "James", age: 28, balance: 400 },
-  //   { inn: 2, name: "David", age: 32, balance: 300 },
-  //   { inn: 3, name: "George", age: 25, balance: 200 },
-  // ];
+// Растения:
+// type     - тип растения (Tree, Bush, Flower)
+// height   - высота растения
+// color    - цвет растения
+// quantity - количество растений
+//
+// copyPlants - флаг для копирования/созданий растений
 
-  // asd = 123;
+class Plants {
   constructor(type, height, color, quantity, copyPlants) {
-    // console.log(3, this.copyClass); //
-    // console.log(4, this.asd); //
-    //
     // *** Паттерн Singleton ***
     // Если экземпляр class уже есть (=  true), то возвращаемся (атрибут instance как маркер)
+
     if (Plants.instance && !copyPlants) {
-      // console.log(2, this.copyClass); //
       return Plants.instance;
     }
     // Иначе создаём новый экземпляр class с атрибутами
-    this.id = 0;
     this.type = type;
     this.height = height;
     this.color = color;
     this.quantity = quantity;
-    // this.copyClass = true; // prototype
-    // let copyClass = false;
+    this.id = 0;
     Plants.instance = this;
   }
 
-  // Генератор id (для тестов паттерна Singleton)
+  // Модуль инкремента id (для тестов паттерна Singleton)
   add() {
     this.id += 1;
   }
 
   // *** Паттерн Prototype ***
+  // Модуль разрешает копирование
   copy() {
-    // this.copyClass = true;
-    // this.asd = 555;
-    // console.log(1, this.copyClass); //
     return new Plants(
-      this.id,
       this.type,
       this.height,
       this.color,
       this.quantity,
-      true
+      true // разрешаем копирование растений
     );
   }
 
-  // Создаём новые растения
+  // Модуль cоздаёт новые растения
   newPlants(type, height, color, quantity) {
     return new Plants(
-      this.id,
       (this.type = type),
       (this.height = height),
       (this.color = color),
       (this.quantity = quantity),
-      true
+      false, // разрешаем создание новый растений
+      (this.id = (Math.random() * 1000).toFixed(0)) // условный генератор id
     );
   }
-  //!   // Метод ищет пользователя
-  //!   findUser(id) {
-  //!     const foundUser = this.user.find((user) => user.id == id);
-  //!     if (foundUser) {
-  //!       return foundUser;
-  //!     }
-  //!     throw new Error("User not found");
-  //!   }
 
-  // Метод добавления растений
+  // Метод добавления числа растений
   addPlants(sum) {
     sum = Math.abs(sum);
-    // const foundUser = this.findUser(userId);
-    // foundUser.balance += money;
     this.quantity += sum;
-    // console.log(foundUser);
   }
 
-  // Метод убавления растений
+  // Метод убавления числа растений
   minusPlants(sum) {
     sum = Math.abs(sum);
-    //  const foundUser = this.findUser(userId);
-    //  foundUser.balance -= money;
     this.quantity -= sum;
-    //  console.log(foundUser);
   }
 
-  // Определяем в какую сторону изменяется значение
+  // Метод определения в какую сторону изменяется значение растений
   changePlants(sum) {
     if (sum > 0) {
       this.addPlants(sum);
@@ -96,79 +74,122 @@ class Plants {
   }
 }
 
-// паттерн Facade
+// *** Паттерн Facade ***
 class PlantsFacade {
   constructor(PlantsInstance) {
     this.PlantsInstance = PlantsInstance;
   }
 
-  // Добавляем растение
+  // Метод добавления новых растений
   addPlants(type, height, color, quantity) {
     this.PlantsInstance.newPlants(type, height, color, quantity);
+    this.msgPlants();
   }
 
-  // Меняем количество растений
+  // Метод изменения количества растений
   change(sum) {
     this.PlantsInstance.changePlants(sum);
+    this.msgPlants();
+  }
+
+  // Метод вывода информации о растениях
+  msgPlants() {
+    console.log(
+      "Facade: растение с id",
+      this.PlantsInstance.id,
+      "типа",
+      this.PlantsInstance.type,
+      ", высотой",
+      this.PlantsInstance.height,
+      ", цвета",
+      this.PlantsInstance.color,
+      "- количество",
+      this.PlantsInstance.quantity
+    );
   }
 }
 
 // Тест # 1
 console.log("# 1");
-console.log("Создаём экземпляры растений");
+// #1.1
+console.log("Создаём экземпляры растений 1 и 2");
 const Plants1 = new Plants("Tree", 5, "green", 10); // Создали экземпляр
 const Plants2 = new Plants(); // Создали ещё один экземпляр, но будет уже созданный
 console.log("Растения 1 id : ", Plants1.id);
 console.log("Растения 2 id : ", Plants2.id);
-console.log("Равны ? : ", Plants1 === Plants2);
-
-console.log("Увеличиваем счётчик растений 1");
+console.log("Растения 1 и 2 равны ? : ", Plants1 === Plants2);
+// #1.2
+console.log("Увеличиваем счётчик id растений 1");
 Plants1.add();
 console.log("Растения 1 id : ", Plants1.id);
 console.log("Растения 2 id : ", Plants2.id);
-console.log("Равны ? : ", Plants1 === Plants2);
-
-console.log("Увеличиваем счётчик растений 2");
+console.log("Растения 1 и 2 равны ? : ", Plants1 === Plants2);
+// #1.3
+console.log("Увеличиваем счётчик id растений 2");
 Plants2.add();
 console.log("Растения 1 id : ", Plants1.id);
 console.log("Растения 2 id : ", Plants2.id);
-console.log("Равны ? : ", Plants1 === Plants2);
+console.log("Растения 1 и 2 равны ? : ", Plants1 === Plants2);
 
 // Тест # 2
 console.log("# 2");
-console.log("Создаём экземпляр Facade");
+console.log("Создаём экземпляр паттерна Facade");
 const plantsInterface = new PlantsFacade(new Plants());
-console.log("Экземпляр Facade : ", plantsInterface);
+console.log("Экземпляр паттерна Facade : ", plantsInterface.PlantsInstance);
 
 // Тест # 3
 console.log("# 3");
+// #3.1
+console.log("Создаём копию растений 3 из 1");
 const Plants3 = Plants1.copy();
 console.log("Растения 1 id : ", Plants1.id);
 console.log("Растения 3 id : ", Plants3.id);
-console.log("Равны ? : ", Plants1 === Plants3);
-
+console.log("Растения 1 и 3 равны ? : ", Plants1 === Plants3);
+// #3.2
 console.log("Меняем тип, высоту, цвет и количество растений 3");
 Plants3.type = "Bush";
 Plants3.height = 1.5;
 Plants3.color = "brow";
 Plants3.quantity = 15;
-console.log("Растения 1 : ", Plants1);
-console.log("Растения 3 : ", Plants3);
+console.log(
+  "Растения 1 : ",
+  "растение с id",
+  Plants1.id,
+  "типа",
+  Plants1.type,
+  ", высотой",
+  Plants1.height,
+  ", цвета",
+  Plants1.color,
+  "- количество",
+  Plants1.quantity
+);
+console.log(
+  "Растения 3 : ",
+  "растение с id",
+  Plants3.id,
+  "типа",
+  Plants3.type,
+  ", высотой",
+  Plants3.height,
+  ", цвета",
+  Plants3.color,
+  "- количество",
+  Plants3.quantity
+);
 
 // Тест # 4
 console.log("# 4");
+// #4.1
 console.log("Работаем с паттерном Facade");
 console.log("Создаём Facade 4");
 const plantsInterface4 = new PlantsFacade(new Plants());
-
-console.log("Facade 4 создаём новые растения");
+// #4.2
+console.log("Создаём новые растения 4 из Facade");
 plantsInterface4.addPlants("Flower", 0.5, "yellow", 25);
-console.log("Экземпляр Facade 4 : ", plantsInterface4);
-
+// #4.3
 console.log("Facade 4 увеличиваем количество растений");
 plantsInterface4.change(20);
-console.log("Экземпляр Facade 4 : ", plantsInterface4);
-
+// #4.4
 console.log("Facade 4 уменьшаем количество растений");
 plantsInterface4.change(-25);
-console.log("Экземпляр Facade 4 : ", plantsInterface4);
